729c8d79232ba0525fec7b4ca394d999e271c389
Tom Bedor <tom@healthloop.com>
2017-02-21 00:02:47 -0800

    update readme, add alternative sequence assembler file

--- README.md
+++ README.md
@@ -1,34 +1,56 @@
-Parse FASTA sequences using Biopyton. To install:
+# Coding Challenge
+
+## Summary
+
+The prompt stated that the collection of subsequences could be combined to form exactly one supersequence. For this 
+exercise, I assumed that the latter portion of each subsequence overlapped the front of exactly one other subsequence, 
+aside from the end fragment. This assumption works if the subsequences are random and long enough. With real genomic
+data, the presence of long repeated sequences might break this assumption.
+
+## Algorithm
+
+The data structure I used was a linked list. For each pair, I tried to overlay a "start" sequence on an "end" sequence.
+If I found a match, I recorded the index of overlap, and removed the "end" sequence from the list of unmatched
+subsequences. The last sequence left from this list was the root of the linked list.
+
+Then, starting from the root, I concatenated each string (truncated at the point of overlap) to form the supersequence. 
+
+To test whether one "start" sequence overlapped with an "end" sequence, I took the first half of the "end" sequence and 
+tested whether it was a substring with the "start" sequence. If it overlapped at character N, I compared the last N 
+characters of the "start" sequence with the first N characters of the "end" sequence.
+ 
+I estimated the run time of this approach to be O(n*n*l), where "n" is the number of sequences, and "l" is the
+approximate length of each sequences. The reasoning is that each string comparison is about O(l) in the worst case, when 
+only the last character differs between the two strings.
+
+## Alternative Approach
+
+I looked into other data structures that could improve on this, and implemented a suffix tree in for string comparison.
+The idea for that was that I stored the sort order of each possible suffix of the "start" sequence. Then, a search for
+the "end" fragment would take O(log l) time with binary search. Since the "end" fragment wouldn't usually be an exact 
+suffix, when doing a binary search I truncated the search string to match the length of the "end" fragment.
+
+This approach ended up being much slower than using Python's native string comparison operator, even without the cost of
+building the suffix tree. I'm not sure why.
+
+## Usage
+
+The script uses BioPython to parse FASTA. To install:
 easy_install -f http://biopython.org/DIST/ biopython
 
+The supersequence is returned by:
+`SequenceAssembler('./FASTA_FILE_NAME.txt').super_sequence()`
 
-bowtie
-MUMmer
+For testing, I used the data from the prompt email, and used the script generate_test_data.py to generate a few more
+test scripts.
+ 
+ ## Files
+ 
+My solution is in sequence_assembler.py. The file can run the larger data set with:
+`python run.py`
 
-For each approach, I'll assume each of n subsequences has a length that is the same order of magnitude: l
+Tests can be run with:
+`python test.py`
 
+The file alternative_sequence_assembler_with_suffix_array.py has the alternative implementation with suffix arrays.
 
-construct a bidirectional graph, and track when a non-match or a match has been found.
-
-
-
-Naive approach - nested loop using native Python string comparators:
-  For each sequence i:
-    For each sequence j:
-      Using str.index(str), If the first half of string i in string j, match and remove strings from pool.
-
-To do this, we must keep track of the minimum overlap distance we can use.
-The native Python "in" operator runs in O(l) time:
-https://wiki.python.org/moin/TimeComplexity
-
-With each round, we'll have one less string to compare, so we'll have O(n!) comparisons to do. Total runtime is:
-O(l*n^2)
-
-
-suffix tree with graph
-
-O(l*log l * n + log l * n^2)
-
-
-
-    
---
+++ alternative_sequence_assembler_with_suffix_array.py
@@ -0,0 +1,100 @@
+from Bio import SeqIO
+class AltSequenceAssembler(object):
+    def __init__(self, input_file):
+        self.raw_sequences = SeqIO.parse(open(input_file),'fasta')
+        self.build_sequence_graph_and_set_root()
+
+    def build_sequence_graph_and_set_root(self):
+        self.sequence_graph = {}
+        for sequence in self.raw_sequences:
+            seq_string = str(sequence.seq)
+            seq_length = len(seq_string)
+            self.sequence_graph[sequence.id] = {
+                    'string':seq_string, 
+                    'next_seq_name': None,
+                    'next_seq_overlap_idx': None,
+                    'suffix_array': SuffixArray(seq_string)
+                    }
+
+        unmatched_end_seq_names= self.sequence_graph.keys()
+        for start_name, start_seq in self.sequence_graph.iteritems():
+            for end_name, end_seq in self.sequence_graph.iteritems():
+                if start_name != end_name and end_name in unmatched_end_seq_names:
+                    overlap_idx = self.get_overlap_idx(start_seq, end_seq)
+                    if overlap_idx != -1:
+                        self.sequence_graph[start_name]['next_seq_name'] = end_name
+                        self.sequence_graph[start_name]['next_seq_overlap_idx'] = overlap_idx
+                        unmatched_end_seq_names.remove(end_name)
+                        break
+        
+        assert(len(unmatched_end_seq_names) == 1)
+        self.root_name =unmatched_end_seq_names[0]
+        return True
+
+    def get_overlap_idx(self, start_seq, end_seq):
+        start_seq_string = start_seq['string']
+        end_seq_string = end_seq['string']
+
+        min_overlap_length = max(len(start_seq_string), len(end_seq_string)) / 2
+        min_overlap_string = end_seq_string[:min_overlap_length]
+
+        min_overlap_index= start_seq['suffix_array'].index(min_overlap_string)
+
+        if min_overlap_index != -1 and len(start_seq_string[min_overlap_index:]) != len(min_overlap_string):
+            full_overlap_string = end_seq_string[:min_overlap_index]
+            return start_seq['suffix_array'].index(full_overlap_string)
+        else:
+            return min_overlap_index
+
+    def super_sequence(self):
+        current_name = self.root_name
+        super_seq = ""
+
+        while current_name:
+            current_seq = self.sequence_graph[current_name]
+            truncate_idx_for_current_string = current_seq['next_seq_overlap_idx']
+            if truncate_idx_for_current_string: 
+                super_seq += current_seq['string'][:truncate_idx_for_current_string]
+            else:
+                super_seq += current_seq['string']
+            current_name = current_seq.get('next_seq_name')
+        return super_seq
+
+
+class SuffixArray(object):
+    def __init__(self, input_string):
+        self.input_string = input_string
+        self.array = range(len(input_string))
+        self.array.sort(key=lambda i: input_string[i:])
+
+    def suffix(self, array_index):
+        string_idx = self.array[array_index]
+        return self.input_string[string_idx:]
+
+    def index(self, query_string, start_bound_array_idx = 0, end_bound_array_idx = None):
+        if end_bound_array_idx is None:
+            end_bound_array_idx = len(self.array) - 1
+        if start_bound_array_idx > end_bound_array_idx:
+            return -1
+
+        search_idx = (end_bound_array_idx + start_bound_array_idx) / 2
+
+        max_length_of_comparison_string = len(query_string)
+        comparison_string = self.suffix(search_idx)[0:max_length_of_comparison_string]
+
+        for idx in range(start_bound_array_idx, end_bound_array_idx + 1):
+            suffix = self.suffix(idx)
+            if comparison_string == suffix:
+                next
+            else:
+                next
+
+        if comparison_string == query_string:
+            return self.array[search_idx]
+        elif start_bound_array_idx == end_bound_array_idx:
+            return -1
+        elif comparison_string < query_string:
+            return self.index(query_string, search_idx + 1, end_bound_array_idx)
+        elif comparison_string > query_string:
+            return self.index(query_string, start_bound_array_idx, search_idx - 1)
+
