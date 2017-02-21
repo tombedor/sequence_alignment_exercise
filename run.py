f6d341f1b647656d12c62d9d3a712252e04d90e2
Tom Bedor <tom@healthloop.com>
2017-02-20 21:59:09 -0800

    Update run and test scripts

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from tree_assembler import SuffixArray
 import timeit
 
 
-SequenceMap('./coding_challenge_data_set.txt').super_sequence()
+print(SequenceMap('./coding_challenge_data_set.txt').super_sequence())
 
