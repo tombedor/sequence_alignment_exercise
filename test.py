6dc8777c0f2b2e83fbade82cb7a825f75ce6d71b
Tom Bedor <tom@healthloop.com>
2017-02-20 21:59:09 -0800

    Update run and test scripts

--- test.py
+++ test.py
@@ -1,21 +1,5 @@
 from tree_assembler import SequenceMap
 from tree_assembler import SuffixArray
 
-mapper = SequenceMap('./test_data_set.txt')
-assert(mapper.super_sequence() == 'ATTAGACCTGCCGGAATAC')
-
-
-suffix_array = SuffixArray('zadc')
-
-assert(suffix_array.array == [1, 3, 2, 0])
-assert(suffix_array.index('adc') == 1)
-
-
-
-assert(suffix_array.index('ffff') == -1)
-assert(suffix_array.index('c') == 3)
-
-
-
-print "Test passes"
-
+assert(SequenceGraph.super_sequence() == 'ATTAGACCTGCCGGAATAC')
+print("Tests pass")
